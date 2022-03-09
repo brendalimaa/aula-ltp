a9f3bad42ea918f255a60aa5f395cb5a1e2acad6
brendalimaa <>
2022-03-09 18:44:17 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+class Retangulo{
+    constructor(altura, largura){
+      this.altura=altura;
+      this.largura=largura;
+    }
+
+  calcularArea(){
+   return this.altura * this.largura
+ }
+  area(){
+    return this.calcularArea();
+  }
+}
+var quadrado = new Retangulo(10,10);
+let retangulo = new Retangulo(30,60);
+console.log(quadrado.calcularArea())
+console.log(retangulo.calcularArea())
+
+
+
+
+
+
+
+
